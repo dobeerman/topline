7778e89a54023f3d8bc92b2ac25e94617bccf79f
Alexander Smirnoff <a.dobeerman@gmail.com>
2018-06-04 10:25:19 +1000

    Fix month

--- src/helpers/parse-date.js
+++ src/helpers/parse-date.js
@@ -4,7 +4,7 @@ const Rmatch = require('ramda/src/match');
 const getDate = date => {
   const [_, d, m, y] = Rmatch(/(^[\d]{2})([\d]{2})([\d].+)/, date);
 
-  return new Date(y, m, d).toLocaleString();
+  return new Date(y, m - 1, d).toLocaleString();
 };
 
 module.exports.getDate = getDate;
